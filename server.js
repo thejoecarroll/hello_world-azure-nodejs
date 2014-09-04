8b39df0559fce5996591f9c7c7b138cd91fab81d
Joe Carroll <joe.carroll@gmail.com>
2014-09-04 13:36:04 +0300

    Compress "Hello Azure" version for testing

--- server.js
+++ server.js
@@ -1,6 +1,1 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Azure\n');
-}).listen(port);
+var http=require("http");var port=process.env.PORT||1337;http.createServer(function(req,res){res.writeHead(200,{"Content-Type":"text/plain"});res.end("Hello Azure\n")}).listen(port);
